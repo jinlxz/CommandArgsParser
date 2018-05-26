3212bca8f89a81bc24b0117978d1a3323987b217
xianzhou.liu <xianzhou.liu@outlook.com>
2018-05-26 19:07:23 +0800

    optimized some code.

--- cmd_test.py
+++ cmd_test.py
@@ -1,53 +1,52 @@
 import sys
-class cmd_arg_struct(object):
-    def __init__(self):
-        self.openfile=None
-        self.target_arg_list=None
-    def __str__(self):
-        return str(self.openfile)+"\n"+str(self.target_arg_list)
 class cmd_arg_parser(object):
     def __init__(self,cmd_args):
         self.index=1
-        self.real_cmd_options=cmd_arg_struct()
+        self.real_cmd_options={}
         self.cmd_args_list=cmd_args
         self.cmd_args_map={
-            "--help":self.display_help,
-            "-h":self.display_help,
-            "--version":self.display_version,
-            "-ver":self.display_version,
-            "--openfile":self.get_opefile_value,
-            "--":self.process_option_separator
+            "--help":(self.display_help,"help"),
+            "-h":(self.display_help,"help"),
+            "--version":(self.display_version,"version"),
+            "-ver":(self.display_version,"version"),
+            "--openfile":(self.process_keyvalue_option,"openfile"),
+            "--enable-smp":(self.process_bool_option,"enable-smp"),
+            "--":(self.process_option_separator,"separator")
         }
-    def get_cmd_map(self,arg):
+    def get_cmd_function(self,arg):
         if self.cmd_args_map.get(arg) is not None:
             return self.cmd_args_map.get(arg)
         else:
             if arg.startswith("-"):
                 print "invalid option {0}".format(arg)
-                return self.display_help
+                return (self.display_help,"help")
             else:
-                return self.get_position_arg_list
-    def display_help(self):
+                return (self.get_position_arg_list,"position_arg_list")
+    def process_bool_option(self,name):
+        self.real_cmd_options[name]=True;
+        self.index+=1
+    def process_keyvalue_option(self,name):
+        self.index+=1
+        self.real_cmd_options[name]=self.cmd_args_list[self.index]
+        self.index+=1
+    def display_help(self,name):
         print "help information"
         sys.exit(0)
-    def display_version(self):
+    def display_version(self,name):
         print "version 1.0"
         sys.exit(0)
-    def get_opefile_value(self):
-        self.index+=1
-        self.real_cmd_options.openfile=self.cmd_args_list[self.index]
-        self.index+=1
-    def get_position_arg_list(self):
-        self.real_cmd_options.target_arg_list=self.cmd_args_list[self.index:]
+    def get_position_arg_list(self,name):
+        self.real_cmd_options[name]=self.cmd_args_list[self.index:]
         self.index=len(self.cmd_args_list)
-    def process_option_separator(self):
+    def process_option_separator(self,name):
         self.index+=1
-        self.get_position_arg_list()
+        self.get_position_arg_list(name)
 
     def parse_cmd_args(self):
         while self.index<len(self.cmd_args_list):
             p_arg = self.cmd_args_list[self.index]
-            self.get_cmd_map(p_arg)()
+            arg_process_func,arg_name=self.get_cmd_function(p_arg)
+            arg_process_func(arg_name)
 if __name__=="__main__":
     cmd_parser=cmd_arg_parser(sys.argv)
     cmd_parser.parse_cmd_args()
